b13410d1eb7d2fad2531e5d70e0e7f2a6d954e5e
VieuxChameau <VieuxChameau@users.noreply.github.com>
2014-10-05 19:13:07 +0200

    Second chapter. Use of link-to and create first controller

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-App = Ember.Application.create({
+var App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 App.Router.map(function () {
+    this.route('credits', { path: '/thanks' });
     this.route('about');
-    this.route('credits');
+});
+App.IndexController = Ember.Controller.extend({
+    productsCount: 42,
+    logo: 'images/logo-small.png',
+    time: function () {
+        return (new Date()).toDateString();
+    }.property()
+});
+
+App.AboutController = Ember.Controller.extend({
+    contactName : 'VieuxChameau',
+    avatar : 'images/avatar.png',
+    open : function(){
+        var currentDay = (new Date()).getDay();
+        if (currentDay === 0) {
+            return "The store is close on Sunday";
+        }
+        return  "Store open";
+    }.property()
 });
