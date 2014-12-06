1a8353731170b4e939f47e203a2135572acef267
VieuxChameau <VieuxChameau@users.noreply.github.com>
2014-12-06 15:42:01 +0100

    Final Chapter

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 var App = Ember.Application.create({
 	LOG_TRANSITIONS: true
 });
-App.Router.map(function() {
-	this.route('credits', { path: '/thanks' });
-	this.resource('products', function() {
-		this.resource('product', { path: '/:product_id' });
+App.Router.map(function () {
+	this.route('credits', {path: '/thanks'});
+	this.resource('products', function () {
+		this.resource('product', {path: '/:product_id'});
 		this.route('onsale');
 		this.route('deals');
 	});
-	this.resource('contacts', function() {
-		this.resource('contact', { path: '/:contact_id' });
+	this.resource('contacts', function () {
+		this.resource('contact', {path: '/:contact_id'});
 	});
 });
 
 App.IndexController = Ember.ArrayController.extend({
 	productsCount: Ember.computed.alias('length'),
 	logo: 'images/logo-small.png',
-	time: function() {
+	time: function () {
 		return (new Date()).toDateString();
 	}.property(),
-	onSale: function() {
-		return this.filterBy('isOnSale').slice(0,3);
+	onSale: function () {
+		return this.filterBy('isOnSale').slice(0, 3);
 	}.property('@each.isOnSale')
 });
 App.ContactsIndexController = Ember.Controller.extend({
 	contactName: 'Anostagia',
 	avatar: 'images/avatar.png',
-	open: function() {
+	open: function () {
 		return ((new Date()).getDay() === 0) ? "Closed" : "Open";
 	}.property()
 });
@@ -44,49 +44,79 @@ App.ReviewsController = Ember.ArrayController.extend({
 App.ContactProductsController = Ember.ArrayController.extend({
 	sortProperties: ['title']
 });
+App.ProductController = Ember.ObjectController.extend({
+	ratings: [1, 2, 3, 4, 5],
+	isNotReviewed: Ember.computed.alias('review.isNew'),
+	review: function () {
+		return this.store.createRecord('review', {
+			product: this.get('model')
+		});
+	}.property('model'),
+	actions: {
+		createReview: function () {
+			var controller = this;
+			this.get('review').set('reviewedAt', new Date());
+			this.get('review').save().then(function (review) {
+				controller.get('model.reviews')
+					.addObject(review);
+			});
+		}
+	}
+});
 
 App.ProductsRoute = Ember.Route.extend({
-	model: function() {
+	model: function () {
 		return this.store.findAll('product');
 	}
 });
 App.ContactsRoute = Ember.Route.extend({
-	model: function() {
+	model: function () {
 		return this.store.findAll('contact');
 	}
 });
 App.IndexRoute = Ember.Route.extend({
-	model: function(){
+	model: function () {
 		return this.store.findAll('product');
 	}
 });
 App.ProductsIndexRoute = Ember.Route.extend({
-	model: function(){
+	model: function () {
 		return this.store.findAll('product');
 	}
 });
 App.ProductsOnsaleRoute = Ember.Route.extend({
-	model: function(){
+	model: function () {
 		return this.modelFor('products').filterBy('isOnSale');
 	}
 });
 App.ProductsDealsRoute = Ember.Route.extend({
-	model: function(){
-		return this.modelFor('products').filter(function(product){
+	model: function () {
+		return this.modelFor('products').filter(function (product) {
 			return product.get('price') < 500;
 		});
 	}
 });
 
+App.ReviewView = Ember.View.extend({
+	isExpanded: false,
+	classNameBindings: ['isExpanded', 'readMore'],
+	click: function () {
+		this.toggleProperty('isExpanded');
+	},
+	readMore: function () {
+		return this.get('length') > 140;
+	}.property('length')
+});
+
 App.ProductDetailsComponent = Ember.Component.extend({
 	reviewsCount: Ember.computed.alias('product.reviews.length'),
-	hasReviews: function(){
+	hasReviews: function () {
 		return this.get('reviewsCount') > 0;
 	}.property('reviewsCount')
 });
 App.ContactDetailsComponent = Ember.Component.extend({
 	productsCount: Ember.computed.alias('contact.products.length'),
-	isProductive: function() {
+	isProductive: function () {
 		return this.get('productsCount') > 3;
 	}.property('productsCount')
 });
@@ -103,18 +133,27 @@ App.Product = DS.Model.extend({
 	description: DS.attr('string'),
 	isOnSale: DS.attr('boolean'),
 	image: DS.attr('string'),
-	reviews: DS.hasMany('review', { async: true }),
-	crafter: DS.belongsTo('contact', { async: true })
+	reviews: DS.hasMany('review', {async: true}),
+	crafter: DS.belongsTo('contact', {async: true}),
+	rating: function () {
+		if (this.get('reviews.length') === 0) {
+			return 0;
+		}
+		return this.get('reviews').reduce(function (previousValue, review) {
+				return previousValue + review.get('rating');
+			}, 0) / this.get('reviews.length');
+	}.property('reviews.@each.rating')
 });
 
 App.Product.FIXTURES = [
-	{  id: 1,
+	{
+		id: 1,
 		title: 'Flint',
 		price: 99,
 		description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
 		isOnSale: true,
 		image: 'images/products/flint.png',
-		reviews: [100,101],
+		reviews: [100, 101],
 		crafter: 200
 	},
 	{
@@ -173,7 +212,7 @@ App.Contact = DS.Model.extend({
 	name: DS.attr('string'),
 	about: DS.attr('string'),
 	avatar: DS.attr('string'),
-	products: DS.hasMany('product', { async: true })
+	products: DS.hasMany('product', {async: true})
 });
 App.Contact.FIXTURES = [
 	{
@@ -181,29 +220,48 @@ App.Contact.FIXTURES = [
 		name: 'Giamia',
 		about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
 		avatar: 'images/contacts/giamia.png',
-		products: [1,4]
+		products: [1, 4]
 	},
 	{
 		id: 201,
 		name: 'Anostagia',
 		about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
 		avatar: 'images/contacts/anostagia.png',
-		products: [2,3,5,6]
+		products: [2, 3, 5, 6]
 	}
 ];
 
 App.Review = DS.Model.extend({
 	text: DS.attr('string'),
 	reviewedAt: DS.attr('date'),
-	product: DS.belongsTo('product')
+	product: DS.belongsTo('product'),
+	rating: DS.attr('number')
 });
 App.Review.FIXTURES = [
 	{
 		id: 100,
-		text: "Started a fire in no time!"
+		reviewedAt: new Date('12/10/2013').getTime(),
+		text: "Started a fire in no time!",
+		rating: 4
 	},
 	{
 		id: 101,
-		text: "Not the brightest flame, but warm!"
+		reviewedAt: new Date('12/12/2013').getTime(),
+		text: "Not the brightest flame, but warm!",
+		rating: 5
+	},
+	{
+		id: 102,
+		reviewedAt: new Date('12/30/2013').getTime(),
+		text: "This is some amazing Flint! It lasts **forever** and works even when damp! I still remember the first day when I was only a little fire sprite and got one of these in my flame stalking for treemas. My eyes lit up the moment I tried it! Here's just a few uses for it:\n\n* Create a fire using just a knife and kindling!\n* Works even after jumping in a lake (although, that's suicide for me)\n* Small enough to fit in a pocket -- if you happen to wear pants\n\n\nYears later I'm still using the _same one_. That's the biggest advantage of this -- it doesn't run out easily like matches. As long as you have something to strike it against, **you can start a fire anywhere** you have something to burn!",
+		rating: 5
 	}
 ];
+
+Ember.Handlebars.registerBoundHelper('markdown', function (text) {
+	return new Handlebars.SafeString(markdown.toHTML(text));
+});
+
+Ember.Handlebars.registerBoundHelper('money', function (value) {
+	return accounting.formatMoney(value / 100);
+});
